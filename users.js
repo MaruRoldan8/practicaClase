671cf2519b89c5cb20236ca670f2229e8d082eee
MaruRoldan8 <mariadelosangeles.rol8@gmail.com>
2023-08-16 16:06:41 -0300

    Creando rama y agregando archivo

---
+++ users.js
@@ -0,0 +1,11 @@
+let user1 = {
+    nombreUsuario: "Adrianadc-Dev",
+    mail: "addiaz802@gmail.com",
+    contrasena: "123456",
+};
+
+let user2 = {
+    nombreUsuario: "MaruRoldan8",
+    mail: "mariadelosangeles.rol8@gmail.com",
+    contrasena: "123456",
+};
